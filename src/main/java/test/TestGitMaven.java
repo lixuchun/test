56e73bebd8c7fab44eaa8a2a68e72326bb9be7dd
lixuchun <1048320921@qq.com>
2016-01-21 16:04:40 +0800

    Update TestGitMaven.java

--- src/main/java/test/TestGitMaven.java
+++ src/main/java/test/TestGitMaven.java
@@ -3,6 +3,7 @@ package test;
 public class TestGitMaven {
     public void main(String[] args) {
         System.out.print("first success");
+        System.out.print("edit the second time");
     }
 
 }
